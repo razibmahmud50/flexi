e58f98e8131700221d357eb8b1b09dfa6c9313fd
razibmahmud50 <razibmahmud50@gmail.com>
2023-06-11 22:08:13 +0600

    color switcher added

--- flexi_html/assets/scripts/color-change.js
+++ flexi_html/assets/scripts/color-change.js
@@ -1,47 +1,34 @@
 /*
  * color change js
- * Author: themebazar
- * Author URL: http://themebazar.com
+ * Author: Razib
+ * Author URL:
  * Version: 1.0
  */
-//theme color changer js
-(function($) {
-    "use strict";
+//toggle color switcher
+(function () {
+    var demoPanel = document.querySelector("#lm_demo_panel"),
+        demoPanelSwitcher = document.querySelector("#lm_demo_panel_switcher");
 
-    var demoPanel = $("#lm_demo_panel"),
-        demoPanelSwitcher = $("#lm_demo_panel_switcher");
-
-    $(window).on("click", function() {
-        $("#lm_demo_panel.active").removeClass("active");
+    window.addEventListener("click", function () {
+        document.querySelector("#lm_demo_panel.active").classList.remove("active");
     });
 
-    demoPanelSwitcher.on("click", function(event) {
+    demoPanelSwitcher.addEventListener("click", function (event) {
         event.stopPropagation();
-        demoPanel.toggleClass("active");
+        demoPanel.classList.toggle("active");
     });
 
-    demoPanel.on("click", function(event) {
+    demoPanel.addEventListener("click", function (event) {
         event.stopPropagation();
     });
 
-    // Template style
-    $('#t_style').on("click", "a.light-theme", function() {
-        $("body").css({
-            '--pri-color': '#F8F8F8',
-            '--bg-color': '#F8F8F8',
-            '--h-color': '#171619',
-            '--white': '#171619',
-        });
-
-    });
-    $('#t_style').on("click", "a.dark-theme", function() {
-        $("body").css({
-            '--pri-color': '#171619',
-            '--bg-color': '#171619',
-            '--h-color': '#fff',
-            '--white': '#fff',
-        });
-
+    // Color switcher
+    document.querySelector('#t_style .light-theme').addEventListener("click", function () {
+        document.querySelector("body").style.cssText = "--pri-color: #F8F8F8; --bg-color: #F8F8F8; --h-color: #171619; --white: #171619;";
     });
 
-})(jQuery);
+    document.querySelector('#t_style .dark-theme').addEventListener("click", function () {
+        document.querySelector("body").style.cssText = "--pri-color: #171619; --bg-color: #171619; --h-color: #fff; --white: #fff;";
+    });
+
+})();
--- flexi_html/assets/scripts/shape.js
+++ flexi_html/assets/scripts/shape.js
@@ -1,33 +1,31 @@
 //shape overlay js file
 //global menu
 //jshint esversion: 6 
-(function($) {
-    "use strict"; {
-        setTimeout(() => document.body.classList.add('render'), 60);
-        const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
-        const total = navdemos.length;
-        const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
-        const navigate = (linkEl) => {
-            document.body.classList.remove('render');
-            document.body.addEventListener('transitionend', () => window.location = linkEl.href);
-        };
-        navdemos.forEach(link => link.addEventListener('click', (ev) => {
-            ev.preventDefault();
-            navigate(ev.target);
-        }));
-        document.addEventListener('keydown', (ev) => {
-            const keyCode = ev.keyCode || ev.which;
-            let linkEl;
-            if (keyCode === 37) {
-                linkEl = current > 0 ? navdemos[current - 1] : navdemos[total - 1];
-            } else if (keyCode === 39) {
-                linkEl = current < total - 1 ? navdemos[current + 1] : navdemos[0];
-            } else {
-                return false;
-            }
-            navigate(linkEl);
-        });
-    }
+(function () {
+    setTimeout(() => document.body.classList.add('render'), 60);
+    const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
+    const total = navdemos.length;
+    const current = navdemos.findIndex(el => el.classList.contains('demo--current'));
+    const navigate = (linkEl) => {
+        document.body.classList.remove('render');
+        document.body.addEventListener('transitionend', () => window.location = linkEl.href);
+    };
+    navdemos.forEach(link => link.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        navigate(ev.target);
+    }));
+    document.addEventListener('keydown', (ev) => {
+        const keyCode = ev.keyCode || ev.which;
+        let linkEl;
+        if (keyCode === 37) {
+            linkEl = current > 0 ? navdemos[current - 1] : navdemos[total - 1];
+        } else if (keyCode === 39) {
+            linkEl = current < total - 1 ? navdemos[current + 1] : navdemos[0];
+        } else {
+            return false;
+        }
+        navigate(linkEl);
+    });
 
     class ShapeOverlays {
         constructor(elm) {
@@ -106,13 +104,30 @@
         }
     }
 
-    (function() {
-        const elm_menubar = document.querySelector('.menu-bar');
-        const gNavItems = document.querySelectorAll('.menu-item');
-        const elmOverlay = document.querySelector('.shape-overlays');
-        const overlay = new ShapeOverlays(elmOverlay);
+    const elm_menubar = document.querySelector('.menu-bar');
+    const gNavItems = document.querySelectorAll('.menu-item');
+    const elmOverlay = document.querySelector('.shape-overlays');
+    const overlay = new ShapeOverlays(elmOverlay);
 
-        elm_menubar.addEventListener('click', () => {
+    elm_menubar.addEventListener('click', () => {
+        if (overlay.isAnimating) {
+            return false;
+        }
+        overlay.toggle();
+        if (overlay.isOpened === true) {
+            elm_menubar.classList.add('is-opened-navi');
+            for (var i = 0; i < gNavItems.length; i++) {
+                gNavItems[i].classList.add('is-opened');
+            }
+        } else {
+            elm_menubar.classList.remove('is-opened-navi');
+            for (var i = 0; i < gNavItems.length; i++) {
+                gNavItems[i].classList.remove('is-opened');
+            }
+        }
+    });
+    gNavItems.forEach(element => {
+        element.addEventListener('click', event => {
             if (overlay.isAnimating) {
                 return false;
             }
@@ -129,22 +144,5 @@
                 }
             }
         });
-        $(".menu-item").on("click", function() {
-            if (overlay.isAnimating) {
-                return false;
-            }
-            overlay.toggle();
-            if (overlay.isOpened === true) {
-                elm_menubar.classList.add('is-opened-navi');
-                for (var i = 0; i < gNavItems.length; i++) {
-                    gNavItems[i].classList.add('is-opened');
-                }
-            } else {
-                elm_menubar.classList.remove('is-opened-navi');
-                for (var i = 0; i < gNavItems.length; i++) {
-                    gNavItems[i].classList.remove('is-opened');
-                }
-            }
-        });
-    }());
-}(jQuery));
+    });
+}());
